13e5ca18274d6214be4d0cd6d1ee53fa38c76042
umer <umer.shahzaddar@subbyx.com>
2026-01-30 15:33:18 +0100

    chore: added submitTimesheetButton

--- tests/apply-and-submit-schedule.spec.ts
+++ tests/apply-and-submit-schedule.spec.ts
@@ -7,15 +7,17 @@ const emailInput = '//input[@id="email"]'
 const passwordInput = '//input[@id="password"]'
 const loginButton = '//*[@data-testid="signInButton"]'
 const timeTrackingMenuItem = '//*[@data-nav-item-id="time-tracking"]'
-const applyDefaultSchedule = '//button[@data-dd-action-name="Apply default schedule"]'
-const confirmationMessage = '//*[text()="Work hours applied successfully"]'
+const applyDefaultScheduleButton = '//button[@data-dd-action-name="Apply default schedule"]'
+const confirmationMessageAlert = '//*[text()="Work hours applied successfully"]'
+const submitTimesheetButton = '//button[@data-dd-action-name="Submit timesheet"]'
 
-test('Apply default schedule', async ({ page }) => {
+test('Apply default schedule and submit', async ({ page }) => {
     await page.goto(baseUrl);
     await page.locator(emailInput).fill(process.env.REMOTE_USER!)
     await page.locator(passwordInput).fill(process.env.REMOTE_PASSWORD!)
     await page.locator(loginButton).click()
     await page.locator(timeTrackingMenuItem).click()
-    await page.locator(applyDefaultSchedule).click()
-    await expect(page.locator(confirmationMessage)).toBeVisible();
+    await page.locator(applyDefaultScheduleButton).click()
+    await expect(page.locator(confirmationMessageAlert)).toBeVisible();
+    await page.locator(submitTimesheetButton).click()
 });
